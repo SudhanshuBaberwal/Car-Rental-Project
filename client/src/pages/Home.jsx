a870da8a430e051117b4382c16764c107d903cc0
SudhanshuBaberwal <24bcs147@iiitdwd.ac.in>
2025-08-14 19:05:18 +0530

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FeatureSection from '../components/FeatureSection'
+import Banner from '../components/Banner'
+import Tesimonial from '../components/Tesimonial'
+import Newslater from '../components/Newslater'
+
+const Home = () => {
+  return (
+    <>
+        <Hero/>
+        <FeatureSection/>
+        <Banner/>
+        <Tesimonial/>
+        <Newslater/>
+    </>
+  )
+}
+
+export default Home
